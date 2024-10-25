5079b88966e024361eae14755aaeeeb5dc82a276
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-10-25 14:52:51 -0600

    Update Practica_11_001.c

--- Practica_11_001.c
+++ Practica_11_001.c
@@ -4,3 +4,42 @@ practica 11 - EJercicio de Examen*/
 include <stdio.h>
 #define apar=10;
 #define comp = 100;
+
+  void ini_matrix (int arr[par][comp])
+  {
+    int i, j;
+    for(i=o; i < apar; i++;)
+    {
+      for(j=0; j < comp; j++)
+      {
+          arr[i][j] = 0;
+      }
+     }  
+    }
+
+  void llenar (arr[10][100])
+  {
+    int i, j, num, numz;
+
+    for(i=0; i<10; i++)
+    {
+      printf("Dame la cantidad: ");
+      scanf("%d", &num);
+      for(j=0; j < num; j++)
+      {
+        printf("DAme el numero de componente: (0-99): ");
+        scanf("%d", &numz);
+        arr[i][numz]=1;
+      }
+    }
+  }
+  void mensual(aparmen[10])
+  {
+    int i;
+    for(i=0; i<10; i++);
+    {
+      printf("numero de aparatos mensual de %d: ");
+      scanf("%d", &aparmen[i]);
+    }
+  }
+
